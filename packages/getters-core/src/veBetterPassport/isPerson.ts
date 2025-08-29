992d74a55d75dadf3818c5239a2e6d05fcec13df
Victor Emanuel <versaujob@gmail.com>
2025-08-29 10:29:20 +0100

    feat: add ve better passport

---
+++ packages/getters-core/src/veBetterPassport/isPerson.ts
@@ -0,0 +1,27 @@
+import { VeBetterPassport__factory } from '@vechain/vebetterdao-contracts';
+
+import { getOrCreateClient } from '../client/utils';
+import { GetterOptions } from '../types/common';
+
+/**
+ * Check if an address is a person in the VeBetterPassport contract
+ * @param user - The user address to check
+ * @param options - Optional client and network configuration
+ * @returns Promise<boolean> - Whether the address is a person
+ */
+export const getIsPerson = async (
+    user: string,
+    options?: GetterOptions,
+): Promise<boolean> => {
+    const enhancedClient = getOrCreateClient(
+        options?.client,
+        options?.networkUrl,
+    );
+
+    return await enhancedClient.contracts
+        .load(
+            enhancedClient.contractAddresses.veBetterPassportContractAddress,
+            VeBetterPassport__factory.abi,
+        )
+        .read.isPerson(user);
+};
