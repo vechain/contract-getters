d3995eeb25c9565e81f81b7377a416d6773e6a2f
Victor Emanuel <versaujob@gmail.com>
2025-08-29 10:28:19 +0100

    feat: add allocation pool

---
+++ packages/getters-core/src/allocationPool/appShares.ts
@@ -0,0 +1,35 @@
+import { XAllocationPool__factory } from '@vechain/vebetterdao-contracts';
+
+import { getOrCreateClient } from '../client/utils';
+import { GetterOptions } from '../types/common';
+
+export type AppShares = {
+    app: string;
+    share: number;
+    unallocatedShare: number;
+};
+
+/**
+ * Get app shares from XAllocationPool contract
+ * @param roundId - The round ID
+ * @param appId - The app ID
+ * @param options - Optional client and network configuration
+ * @returns Promise<{ share: string; unallocatedShare: string }> - The app shares
+ */
+export const getAppShares = async (
+    roundId: string,
+    appId: string,
+    options?: GetterOptions,
+) => {
+    const enhancedClient = getOrCreateClient(
+        options?.client,
+        options?.networkUrl,
+    );
+
+    return await enhancedClient.contracts
+        .load(
+            enhancedClient.contractAddresses.xAllocationPoolContractAddress,
+            XAllocationPool__factory.abi,
+        )
+        .read.getAppShares(BigInt(roundId), appId as `0x${string}`);
+};
